18eb4d877a94c61304039e51681fe18544478864
Fellipe Horta Parreiras <fhparreiras@gmail.com>
2022-03-06 21:33:21 -0300

    Requisito 3 em andamento

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import walletLogo from '../img/cash-flow.png';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    console.log(email);
+    return (
+      <div className="container-header">
+        <img src={ walletLogo } alt="Logo carteira" width="60" />
+        <span className="header-title">TrybeWallet</span>
+        <span className="header-email" data-testid="email-field">
+          { email }
+        </span>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Wallet);
